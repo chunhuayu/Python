dc739f3f3b8d0730d16f9390632d8e6094e3c0b7
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-10 22:22:04 -0400

    Create Dict &  Booleans & Tuple.py

---
+++ Crash Course/Dict &  Booleans & Tuple.py
@@ -0,0 +1,33 @@
+# ### Dictionaries
+
+>>> d = {'key1':'item1','key2':'item2'}
+>>> d
+{'key1':'item1','key2':'item2'}
+
+>>> d['key1']
+'item1'
+
+# ### Booleans
+
+>>> True
+True
+
+>>> False
+False
+
+# ### Tuples 
+
+>>> t = (1,2,3)
+>>> t[0]
+1
+
+# Tuple is immutable
+>>> t[0] = 'NEW'
+
+---------------------------------------------------------------------------
+TypeError                                 Traceback (most recent call last)
+<ipython-input-44-97e4e33b36c2> in <module>()
+----> 1 t[0] = 'NEW'
+
+TypeError: 'tuple' object does not support item assignment
+
