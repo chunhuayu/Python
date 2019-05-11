c9220aa64b40c4d54898cbb8ac2a8c426cc8dc0f
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-10 22:07:03 -0400

    Update and rename 03-Python Crash Course Exercises - Solutions.py to Python Crash Course Exercises - Solutions.py

--- Crash Course/Python Crash Course Exercises - Solutions.py
+++ Crash Course/Python Crash Course Exercises - Solutions.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# ___
-# 
-# <a href='http://www.pieriandata.com'> <img src='../Pierian_Data_Logo.png' /></a>
-# ___
 # # Python Crash Course Exercises - Solutions
 # 
 # This is an optional exercise to test your understanding of Python Basics. If you find this extremely challenging, then you probably are not ready for the rest of this course yet and don't have enough programming experience to continue. I would suggest you take another course more geared towards complete beginners, such as [Complete Python Bootcamp](https://www.udemy.com/complete-python-bootcamp/?couponCode=PY20)
@@ -15,10 +9,7 @@
 
 # ** What is 7 to the power of 4?**
 
-# In[1]:
-
-
-7**4
+>>> 7**4
 
 
 # ** Split this string:**
@@ -27,16 +18,9 @@
 #     
 # **into a list. **
 
-# In[4]:
+>>> s = 'Hi there Sam!'
 
-
-s = 'Hi there Sam!'
-
-
-# In[3]:
-
-
-s.split()
+>>> s.split()
 
 
 # ** Given the variables:**
@@ -48,52 +32,28 @@ s.split()
 # 
 #     The diameter of Earth is 12742 kilometers.
 
-# In[5]:
+>>> planet = "Earth"
+>>> diameter = 12742
 
 
-planet = "Earth"
-diameter = 12742
-
-
-# In[6]:
-
-
-print("The diameter of {} is {} kilometers.".format(planet,diameter))
+>>> print("The diameter of {} is {} kilometers.".format(planet,diameter))
 
 
 # ** Given this nested list, use indexing to grab the word "hello" **
 
-# In[7]:
-
-
-lst = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
-
-
-# In[14]:
-
-
-lst[3][1][2][0]
+>>> lst = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
+>>> lst[3][1][2][0]
 
 
 # ** Given this nest dictionary grab the word "hello". Be prepared, this will be annoying/tricky **
 
-# In[16]:
+>>> d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
 
 
-d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
-
-
-# In[22]:
-
-
-d['k1'][3]['tricky'][3]['target'][3]
+>>> d['k1'][3]['tricky'][3]['target'][3]
 
 
 # ** What is the main difference between a tuple and a list? **
-
-# In[23]:
-
-
 # Tuple is immutable
 
 
@@ -103,39 +63,21 @@ d['k1'][3]['tricky'][3]['target'][3]
 #     
 # **So for example, passing "user@domain.com" would return: domain.com**
 
-# In[24]:
-
-
-def domainGet(email):
-    return email.split('@')[-1]
-
-
-# In[26]:
-
-
-domainGet('user@domain.com')
+>>> def domainGet(email): return email.split('@')[-1]
+>>> domainGet('user@domain.com')
 
 
 # ** Create a basic function that returns True if the word 'dog' is contained in the input string. Don't worry about edge cases like a punctuation being attached to the word dog, but do account for capitalization. **
 
-# In[27]:
-
-
-def findDog(st):
-    return 'dog' in st.lower().split()
-
-
-# In[28]:
-
-
-findDog('Is there a dog here?')
+>>> def findDog(st): return 'dog' in st.lower().split()
+>>> findDog('Is there a dog here?')
 
 
 # ** Create a function that counts the number of times the word "dog" occurs in a string. Again ignore edge cases. **
 
-# In[30]:
 
 
+>>>
 def countDog(st):
     count = 0
     for word in st.lower().split():
@@ -143,11 +85,7 @@ def countDog(st):
             count += 1
     return count
 
-
-# In[31]:
-
-
-countDog('This dog runs faster than the other dog dude!')
+>>> countDog('This dog runs faster than the other dog dude!')
 
 
 # ** Use lambda expressions and the filter() function to filter out words from a list that don't start with the letter 's'. For example:**
@@ -158,16 +96,10 @@ countDog('This dog runs faster than the other dog dude!')
 # 
 #     ['soup','salad']
 
-# In[34]:
+>>> seq = ['soup','dog','salad','cat','great']
 
 
-seq = ['soup','dog','salad','cat','great']
-
-
-# In[35]:
-
-
-list(filter(lambda word: word[0]=='s',seq))
+>>> list(filter(lambda word: word[0]=='s',seq))
 
 
 # ### Final Problem
@@ -177,9 +109,7 @@ list(filter(lambda word: word[0]=='s',seq))
 #   and 80 inclusive, the result is "Small Ticket". If speed is 81 or more, the result is "Big    Ticket". Unless it is your birthday (encoded as a boolean value in the parameters of the function) -- on your birthday, your speed can be 5 higher in all 
 #   cases. **
 
-# In[4]:
-
-
+>>> 
 def caught_speeding(speed, is_birthday):
     
     if is_birthday:
@@ -194,17 +124,8 @@ def caught_speeding(speed, is_birthday):
     else:
         return 'No Ticket'
 
-
-# In[5]:
-
-
-caught_speeding(81,True)
+>>> caught_speeding(81,True)
 
 
-# In[6]:
+>>> caught_speeding(81,False)
 
-
-caught_speeding(81,False)
-
-
-# # Great job!
