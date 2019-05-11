5ca8b0ed44d7a10756aae0b2d04bc631af858746
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-10 22:31:15 -0400

    Create if elif else.py

---
+++ Crash Course/if elif else.py
@@ -0,0 +1,29 @@
+### if,elif, else Statements
+
+>>> if 1 < 2:
+        print('Yep!')
+Yep!
+
+>>> if 1 < 2:
+        print('yep!')
+yep!
+
+>>> if 1 < 2:
+         print('first')
+     else:
+         print('last')
+first
+
+>>> if 1 > 2:
+        print('first')
+    else:
+        print('last')
+last
+
+>>> if 1 == 2:
+        print('first')
+    elif 3 == 3:
+        print('middle')
+    else:
+        print('Last')
+middle
