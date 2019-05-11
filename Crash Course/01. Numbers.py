0fb4e1d436e27834e5420f3f95489bdba1711358
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-10 23:58:18 -0400

    Update and rename 1. Numbers.py to 01. Numbers.py

---
+++ Crash Course/01. Numbers.py
@@ -0,0 +1,37 @@
+# There are three numeric types in Python:
+# int
+# float
+# complex
+# Variables of numeric types are created when you assign a value to them:
+>>> x = 1    # int
+>>> y = 2.8  # float
+>>> z = 1j   # complex
+>>> print(type(x))
+<class 'int'>
+
+>>> print(type(y))
+<class 'float'>
+
+>>> print(type(z))
+<class 'complex'>
+
+>>> 1 + 1
+2
+
+>>> 1 * 3
+3
+
+>>> 1 / 2
+0.5
+
+>>> 2 ** 4
+16
+
+>>> 4 % 2
+0
+
+>>> 5 % 2
+1
+
+>>> (2 + 3) * (5 + 5)
+5
