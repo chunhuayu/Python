29d2753769ba13044bd17baf39e408819c674575
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-10 23:50:13 -0400

    Update and rename 2. Variable Assignment.py to 02. Variable Assignment.py

---
+++ Crash Course/02. Variable Assignment.py
@@ -0,0 +1,13 @@
+# Can not start with number or special characters. 
+# Variable Names
+# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+>>> name_of_var = 2
+>>> x = 2
+>>> y = 3
+>>> z = x + y
+>>> z
+5
--- Crash Course/2. Variable Assignment.py
+++
@@ -1,7 +0,0 @@
-# Can not start with number or special characters. 
->>> name_of_var = 2
->>> x = 2
->>> y = 3
->>> z = x + y
->>> z
-5
