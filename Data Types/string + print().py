eb856449815a14ba90263e8e99b21490d36676a0
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-10 21:20:27 -0400

    Create string + print().py

---
+++ Data Types/string + print().py
@@ -0,0 +1,40 @@
+>>> "Hello, world!"
+'Hello, world!'
+
+>>> "Let's go"
+"Let's go"
+
+>>> 'Let\'s go'
+"Let's go"
+
+>>> "\"Hello, world!\" She said "
+' "Hello, world!" She said'
+
+>>> x = 'Hello, '
+>>> x
+'Hello, '
+
+>>> y = 'world!'
+>>> y
+'world!'
+
+>>> x+y
+'Hello, world!'
+
+>>> 'Hello,\nworld!'
+'Hello,\nworld!'
+
+>>> print('Hello,\nworld!')
+Hello,
+world!
+
+>>> print(x)
+Hello, 
+
+>>> num = 12
+>>> name = 'Sam'
+>>> print('My number is: {one}, and my name is: {two}'.format(one=num,two=name))
+My number is: 12, and my name is: Sam
+
+>>> print('My number is: {}, and my name is: {}'.format(num,name))
+My number is: 12, and my name is: Sam
