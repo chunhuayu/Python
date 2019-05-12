34d3fa2d93ff2a4a0f2f3069f71c0982e6d3a013
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-11 20:31:48 -0400

    Update 17. Dates.py

--- Crash Course/17. Dates.py
+++ Crash Course/17. Dates.py
@@ -1,61 +1,50 @@
-Python Dates
-A date in Python is not a data type of its own, but we can import a module named datetime to work with dates as date objects.
+# Python Dates
+# A date in Python is not a data type of its own, but we can import a module named datetime to work with dates as date objects.
 
-Example
-Import the datetime module and display the current date:
+# Import the datetime module and display the current date:
 
-import datetime
+>>> import datetime
+>>> x = datetime.datetime.now()
+>>> print(x)
+2019-05-11 20:31:30.936846
 
-x = datetime.datetime.now()
-print(x)
-Run example »
-Date Output
-When we execute the code from the example above the result will be:
+# Date Output
+# When we execute the code from the example above the result will be:
 
-2019-05-11 14:37:01.297412
-The date contains year, month, day, hour, minute, second, and microsecond.
+# 2019-05-11 14:37:01.297412
+# The date contains year, month, day, hour, minute, second, and microsecond.
 
-The datetime module has many methods to return information about the date object.
+# The datetime module has many methods to return information about the date object.
 
-Here are a few examples, you will learn more about them later in this chapter:
+# Here are a few examples, you will learn more about them later in this chapter:
 
-Example
-Return the year and name of weekday:
+# Return the year and name of weekday:
 
-import datetime
+>>> import datetime
+>>> x = datetime.datetime.now()
+>>> print(x.year)
+>>> print(x.strftime("%A"))
+2019
+Saturday
 
-x = datetime.datetime.now()
+# Creating Date Objects
+# To create a date, we can use the datetime() class (constructor) of the datetime module.
+# The datetime() class requires three parameters to create a date: year, month, day.
+# Create a date object:
 
-print(x.year)
-print(x.strftime("%A"))
-Run example »
-Creating Date Objects
-To create a date, we can use the datetime() class (constructor) of the datetime module.
+>>> import datetime
+>>> x = datetime.datetime(2020, 5, 17)
+>>> print(x)
+2020-05-17 00:00:00
 
-The datetime() class requires three parameters to create a date: year, month, day.
+# The datetime() class also takes parameters for time and timezone (hour, minute, second, microsecond, tzone), but they are optional, and has a default value of 0, (None for timezone).
+# The strftime() Method
+# The datetime object has a method for formatting date objects into readable strings.
 
-Example
-Create a date object:
+# The method is called strftime(), and takes one parameter, format, to specify the format of the returned string:
+# Display the name of the month:
 
-import datetime
-
-x = datetime.datetime(2020, 5, 17)
-
-print(x)
-Run example »
-The datetime() class also takes parameters for time and timezone (hour, minute, second, microsecond, tzone), but they are optional, and has a default value of 0, (None for timezone).
-
-
-The strftime() Method
-The datetime object has a method for formatting date objects into readable strings.
-
-The method is called strftime(), and takes one parameter, format, to specify the format of the returned string:
-
-Example
-Display the name of the month:
-
-import datetime
-
-x = datetime.datetime(2018, 6, 1)
-
-print(x.strftime("%B"))
+>>> import datetime
+>>> x = datetime.datetime(2018, 6, 1)
+>>> print(x.strftime("%B"))
+June
