da869d2c2fe26a562956d561cdc0b17ae10440fb
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-13 00:00:14 -0400

    Update 06. Indexing, slicing and Iterating.py

--- 01. Numpy/06. Indexing, slicing and Iterating.py
+++ 01. Numpy/06. Indexing, slicing and Iterating.py
@@ -27,7 +27,9 @@ nan
 7.0
 8.0
 9.0
-Multidimensional arrays can have one index per axis. These indices are given in a tuple separated by commas:
+
+
+# Multidimensional arrays can have one index per axis. These indices are given in a tuple separated by commas:
 
 >>>
 >>> def f(x,y):
@@ -50,6 +52,233 @@ array([ 1, 11, 21, 31, 41])
 array([[10, 11, 12, 13],
        [20, 21, 22, 23]])
 
+# Indexing with Arrays of Indices
+
+>>>
+>>> a = np.arange(12)**2                       # the first 12 square numbers
+>>> i = np.array( [ 1,1,3,8,5 ] )              # an array of indices
+>>> a[i]                                       # the elements of a at the positions i
+array([ 1,  1,  9, 64, 25])
+
+
+>>>
+>>> j = np.array( [ [ 3, 4], [ 9, 7 ] ] )      # a bidimensional array of indices
+>>> a[j]                                       # the same shape as j
+array([[ 9, 16],
+       [81, 49]])
+
+
+# When the indexed array a is multidimensional, a single array of indices refers to the first dimension of a. 
+# The following example shows this behavior by converting an image of labels into a color image using a palette.
+
+>>>
+>>> palette = np.array( [ [0,0,0],                # black
+...                       [255,0,0],              # red
+...                       [0,255,0],              # green
+...                       [0,0,255],              # blue
+...                       [255,255,255] ] )       # white
+>>> image = np.array( [ [ 0, 1, 2, 0 ],           # each value corresponds to a color in the palette
+...                     [ 0, 3, 4, 0 ]  ] )
+>>> palette[image]                            # the (2,4,3) color image
+array([[[  0,   0,   0],
+        [255,   0,   0],
+        [  0, 255,   0],
+        [  0,   0,   0]],
+       [[  0,   0,   0],
+        [  0,   0, 255],
+        [255, 255, 255],
+        [  0,   0,   0]]])
+
+
+# We can also give indexes for more than one dimension. The arrays of indices for each dimension must have the same shape.
+
+>>>
+>>> a = np.arange(12).reshape(3,4)
+>>> a
+array([[ 0,  1,  2,  3],
+       [ 4,  5,  6,  7],
+       [ 8,  9, 10, 11]])
+>>> i = np.array( [ [0,1],                        # indices for the first dim of a
+...                 [1,2] ] )
+>>> j = np.array( [ [2,1],                        # indices for the second dim
+...                 [3,3] ] )
+>>>
+>>> a[i,j]                                     # i and j must have equal shape
+array([[ 2,  5],
+       [ 7, 11]])
+>>>
+>>> a[i,2]
+array([[ 2,  6],
+       [ 6, 10]])
+>>>
+>>> a[:,j]                                     # i.e., a[ : , j]
+array([[[ 2,  1],
+        [ 3,  3]],
+       [[ 6,  5],
+        [ 7,  7]],
+       [[10,  9],
+        [11, 11]]])
+
+
+# Naturally, we can put i and j in a sequence (say a list) and then do the indexing with the list.
+
+>>>
+>>> l = [i,j]
+>>> a[l]                                       # equivalent to a[i,j]
+array([[ 2,  5],
+       [ 7, 11]])
+
+
+# However, we can not do this by putting i and j into an array, because this array will be interpreted as indexing 
+# the first dimension of a.
+>>>
+>>> s = np.array( [i,j] )
+>>> a[s]                                       # not what we want
+Traceback (most recent call last):
+  File "<stdin>", line 1, in ?
+IndexError: index (3) out of range (0<=index<=2) in dimension 0
+>>>
+>>> a[tuple(s)]                                # same as a[i,j]
+array([[ 2,  5],
+       [ 7, 11]])
+
+# Another common use of indexing with arrays is the search of the maximum value of time-dependent series:
+>>>
+>>> time = np.linspace(20, 145, 5)                 # time scale
+>>> data = np.sin(np.arange(20)).reshape(5,4)      # 4 time-dependent series
+>>> time
+array([  20.  ,   51.25,   82.5 ,  113.75,  145.  ])
+>>> data
+array([[ 0.        ,  0.84147098,  0.90929743,  0.14112001],
+       [-0.7568025 , -0.95892427, -0.2794155 ,  0.6569866 ],
+       [ 0.98935825,  0.41211849, -0.54402111, -0.99999021],
+       [-0.53657292,  0.42016704,  0.99060736,  0.65028784],
+       [-0.28790332, -0.96139749, -0.75098725,  0.14987721]])
+>>>
+>>> ind = data.argmax(axis=0)                  # index of the maxima for each series
+>>> ind
+array([2, 0, 3, 1])
+>>>
+>>> time_max = time[ind]                       # times corresponding to the maxima
+>>>
+>>> data_max = data[ind, range(data.shape[1])] # => data[ind[0],0], data[ind[1],1]...
+>>>
+>>> time_max
+array([  82.5 ,   20.  ,  113.75,   51.25])
+>>> data_max
+array([ 0.98935825,  0.84147098,  0.99060736,  0.6569866 ])
+>>>
+>>> np.all(data_max == data.max(axis=0))
+True
+
+
+# You can also use indexing with arrays as a target to assign to:
+>>>
+>>> a = np.arange(5)
+>>> a
+array([0, 1, 2, 3, 4])
+>>> a[[1,3,4]] = 0
+>>> a
+array([0, 0, 2, 0, 0])
+
+
+# However, when the list of indices contains repetitions, the assignment is done several times, leaving behind the last value:
+>>>
+>>> a = np.arange(5)
+>>> a[[0,0,2]]=[1,2,3]
+>>> a
+array([2, 1, 3, 3, 4])
+
+
+# This is reasonable enough, but watch out if you want to use Python’s += construct, as it may not do what you expect:
+>>>
+>>> a = np.arange(5)
+>>> a[[0,0,2]]+=1
+>>> a
+array([1, 1, 3, 3, 4])
+
+
+# Even though 0 occurs twice in the list of indices, the 0th element is only incremented once. 
+# This is because Python requires “a+=1” to be equivalent to “a = a + 1”.
+
+# Indexing with Boolean Arrays
+# When we index arrays with arrays of (integer) indices we are providing the list of indices to pick. 
+# With boolean indices the approach is different; we explicitly choose which items in the array we want and which ones we don’t.
+# The most natural way one can think of for boolean indexing is to use boolean arrays that have the same shape as the original array:
+
+>>>
+>>> a = np.arange(12).reshape(3,4)
+>>> b = a > 4
+>>> b                                          # b is a boolean with a's shape
+array([[False, False, False, False],
+       [False,  True,  True,  True],
+       [ True,  True,  True,  True]])
+>>> a[b]                                       # 1d array with the selected elements
+array([ 5,  6,  7,  8,  9, 10, 11])
+This property can be very useful in assignments:
+
+>>>
+>>> a[b] = 0                                   # All elements of 'a' higher than 4 become 0
+>>> a
+array([[0, 1, 2, 3],
+       [4, 0, 0, 0],
+       [0, 0, 0, 0]])
+
+
+# You can look at the following example to see how to use boolean indexing to generate an image of the Mandelbrot set:
+
+>>>
+>>> import numpy as np
+>>> import matplotlib.pyplot as plt
+>>> def mandelbrot( h,w, maxit=20 ):
+...     """Returns an image of the Mandelbrot fractal of size (h,w)."""
+...     y,x = np.ogrid[ -1.4:1.4:h*1j, -2:0.8:w*1j ]
+...     c = x+y*1j
+...     z = c
+...     divtime = maxit + np.zeros(z.shape, dtype=int)
+...
+...     for i in range(maxit):
+...         z = z**2 + c
+...         diverge = z*np.conj(z) > 2**2            # who is diverging
+...         div_now = diverge & (divtime==maxit)  # who is diverging now
+...         divtime[div_now] = i                  # note when
+...         z[diverge] = 2                        # avoid diverging too much
+...
+...     return divtime
+>>> plt.imshow(mandelbrot(400,400))
+>>> plt.show()
+../_images/quickstart-1.png
+
+
+# The second way of indexing with booleans is more similar to integer indexing; 
+# for each dimension of the array we give a 1D boolean array selecting the slices we want:
+
+>>>
+>>> a = np.arange(12).reshape(3,4)
+>>> b1 = np.array([False,True,True])             # first dim selection
+>>> b2 = np.array([True,False,True,False])       # second dim selection
+>>>
+>>> a[b1,:]                                   # selecting rows
+array([[ 4,  5,  6,  7],
+       [ 8,  9, 10, 11]])
+>>>
+>>> a[b1]                                     # same thing
+array([[ 4,  5,  6,  7],
+       [ 8,  9, 10, 11]])
+>>>
+>>> a[:,b2]                                   # selecting columns
+array([[ 0,  2],
+       [ 4,  6],
+       [ 8, 10]])
+>>>
+>>> a[b1,b2]                                  # a weird thing to do
+array([ 4, 10])
+
+
+
+# Note that the length of the 1D boolean array must coincide with the length of the dimension (or axis) you want to slice.
+# In the previous example, b1 has length 3 (the number of rows in a), and b2 (of length 4) is suitable to 
+# index the 2nd axis (columns) of a.
 
 # When fewer indices are provided than the number of axes, the missing indices are considered complete slices:
 
