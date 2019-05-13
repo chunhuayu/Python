3207c59fa65dafd746f23fc94ce879003e6f80af
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-13 00:14:53 -0400

    Create 12. Linear Algebra.py

---
+++ 01. Numpy/12. Linear Algebra.py
@@ -0,0 +1,85 @@
+# Linear Algebra
+# Work in progress. Basic linear algebra to be included here.
+
+# Simple Array Operations
+# See linalg.py in numpy folder for more.
+
+>>>
+>>> import numpy as np
+>>> a = np.array([[1.0, 2.0], [3.0, 4.0]])
+>>> print(a)
+[[ 1.  2.]
+ [ 3.  4.]]
+
+>>> a.transpose()
+array([[ 1.,  3.],
+       [ 2.,  4.]])
+
+>>> np.linalg.inv(a)
+array([[-2. ,  1. ],
+       [ 1.5, -0.5]])
+
+>>> u = np.eye(2) # unit 2x2 matrix; "eye" represents "I"
+>>> u
+array([[ 1.,  0.],
+       [ 0.,  1.]])
+>>> j = np.array([[0.0, -1.0], [1.0, 0.0]])
+
+>>> j @ j        # matrix product
+array([[-1.,  0.],
+       [ 0., -1.]])
+
+>>> np.trace(u)  # trace
+2.0
+
+>>> y = np.array([[5.], [7.]])
+>>> np.linalg.solve(a, y)
+array([[-3.],
+       [ 4.]])
+
+>>> np.linalg.eig(j)
+(array([ 0.+1.j,  0.-1.j]), array([[ 0.70710678+0.j        ,  0.70710678-0.j        ],
+       [ 0.00000000-0.70710678j,  0.00000000+0.70710678j]]))
+Parameters:
+    square matrix
+Returns
+    The eigenvalues, each repeated according to its multiplicity.
+    The normalized (unit "length") eigenvectors, such that the
+    column ``v[:,i]`` is the eigenvector corresponding to the
+    eigenvalue ``w[i]`` .
+
+# Tricks and Tips
+# “Automatic” Reshaping
+# To change the dimensions of an array, you can omit one of the sizes which will then be deduced automatically:
+
+>>>
+>>> a = np.arange(30)
+>>> a.shape = 2,-1,3  # -1 means "whatever is needed"
+>>> a.shape
+(2, 5, 3)
+>>> a
+array([[[ 0,  1,  2],
+        [ 3,  4,  5],
+        [ 6,  7,  8],
+        [ 9, 10, 11],
+        [12, 13, 14]],
+       [[15, 16, 17],
+        [18, 19, 20],
+        [21, 22, 23],
+        [24, 25, 26],
+        [27, 28, 29]]])
+
+# Vector Stacking
+# How do we construct a 2D array from a list of equally-sized row vectors? 
+# In MATLAB this is quite easy: if x and y are two vectors of the same length you only need do m=[x;y]. 
+# In NumPy this works via the functions column_stack, dstack, hstack and vstack, 
+# depending on the dimension in which the stacking is to be done. For example:
+
+x = np.arange(0,10,2)                     # x=([0,2,4,6,8])
+y = np.arange(5)                          # y=([0,1,2,3,4])
+m = np.vstack([x,y])                      # m=([[0,2,4,6,8],
+                                          #     [0,1,2,3,4]])
+xy = np.hstack([x,y])                     # xy =([0,2,4,6,8,0,1,2,3,4])
+
+
+# The logic behind those functions in more than two dimensions can be strange.
