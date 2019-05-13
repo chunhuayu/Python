2d428b9ddff979f86b78f44dd829c92fe90891ab
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-12 22:24:21 -0400

    Create 03. Basic Operations.py

---
+++ 01. Numpy/03. Basic Operations.py
@@ -0,0 +1,112 @@
+# Basic Operations
+# Arithmetic operators on arrays apply elementwise. A new array is created and filled with the result.
+>>>
+>>> a = np.array( [20,30,40,50] )
+>>> b = np.arange( 4 )
+>>> b
+array([0, 1, 2, 3])
+>>> c = a-b
+>>> c
+array([20, 29, 38, 47])
+>>> b**2
+array([0, 1, 4, 9])
+>>> 10*np.sin(a)
+array([ 9.12945251, -9.88031624,  7.4511316 , -2.62374854])
+>>> a<35
+array([ True, True, False, False])
+
+
+
+# Unlike in many matrix languages, the product operator * operates elementwise in NumPy arrays. 
+# The matrix product can be performed using the @ operator (in python >=3.5) or the dot function or method:
+
+>>>
+>>> A = np.array( [[1,1],
+...             [0,1]] )
+>>> B = np.array( [[2,0],
+...             [3,4]] )
+>>> A * B                       # elementwise product
+array([[2, 0],
+       [0, 4]])
+>>> A @ B                       # matrix product
+array([[5, 4],
+       [3, 4]])
+>>> A.dot(B)                    # another matrix product
+array([[5, 4],
+       [3, 4]])
+
+
+# Some operations, such as += and *=, act in place to modify an existing array rather than create a new one.
+
+>>>
+>>> a = np.ones((2,3), dtype=int)
+>>> b = np.random.random((2,3))
+>>> a *= 3
+>>> a
+array([[3, 3, 3],
+       [3, 3, 3]])
+>>> b += a
+>>> b
+array([[ 3.417022  ,  3.72032449,  3.00011437],
+       [ 3.30233257,  3.14675589,  3.09233859]])
+>>> a += b                  # b is not automatically converted to integer type
+Traceback (most recent call last):
+  ...
+TypeError: Cannot cast ufunc add output from dtype('float64') to dtype('int64') with casting rule 'same_kind'
+
+
+# When operating with arrays of different types, the type of the resulting array corresponds to the more general 
+# or precise one (a behavior known as upcasting).
+
+>>>
+>>> a = np.ones(3, dtype=np.int32)
+>>> b = np.linspace(0,pi,3)
+>>> b.dtype.name
+'float64'
+>>> c = a+b
+>>> c
+array([ 1.        ,  2.57079633,  4.14159265])
+>>> c.dtype.name
+'float64'
+>>> d = np.exp(c*1j)
+>>> d
+array([ 0.54030231+0.84147098j, -0.84147098+0.54030231j,
+       -0.54030231-0.84147098j])
+>>> d.dtype.name
+'complex128'
+
+
+# Many unary operations, such as computing the sum of all the elements in the array, 
+# are implemented as methods of the ndarray class.
+>>>
+>>> a = np.random.random((2,3))
+>>> a
+array([[ 0.18626021,  0.34556073,  0.39676747],
+       [ 0.53881673,  0.41919451,  0.6852195 ]])
+>>> a.sum()
+2.5718191614547998
+>>> a.min()
+0.1862602113776709
+>>> a.max()
+0.6852195003967595
+
+
+# By default, these operations apply to the array as though it were a list of numbers, regardless of its shape. 
+# However, by specifying the axis parameter you can apply an operation along the specified axis of an array:
+>>>
+>>> b = np.arange(12).reshape(3,4)
+>>> b
+array([[ 0,  1,  2,  3],
+       [ 4,  5,  6,  7],
+       [ 8,  9, 10, 11]])
+>>>
+>>> b.sum(axis=0)                            # sum of each column
+array([12, 15, 18, 21])
+>>>
+>>> b.min(axis=1)                            # min of each row
+array([0, 4, 8])
+>>>
+>>> b.cumsum(axis=1)                         # cumulative sum along each row
+array([[ 0,  1,  3,  6],
+       [ 4,  9, 15, 22],
+       [ 8, 17, 27, 38]])
