cf871a80632278f3b7e19d00c315808431410dca
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-12 23:44:17 -0400

    Create 09. Splitting.py

---
+++ 01. Numpy/09. Splitting.py
@@ -0,0 +1,22 @@
+# Splitting one array into several smaller ones
+
+# Using hsplit, you can split an array along its horizontal axis, 
+# either by specifying the number of equally shaped arrays to return, or by specifying the columns after which the division should occur:
+
+>>>
+>>> a = np.floor(10*np.random.random((2,12)))
+>>> a
+array([[ 9.,  5.,  6.,  3.,  6.,  8.,  0.,  7.,  9.,  7.,  2.,  7.],
+       [ 1.,  4.,  9.,  2.,  2.,  1.,  0.,  6.,  2.,  2.,  4.,  0.]])
+>>> np.hsplit(a,3)   # Split a into 3
+[array([[ 9.,  5.,  6.,  3.],
+       [ 1.,  4.,  9.,  2.]]), array([[ 6.,  8.,  0.,  7.],
+       [ 2.,  1.,  0.,  6.]]), array([[ 9.,  7.,  2.,  7.],
+       [ 2.,  2.,  4.,  0.]])]
+>>> np.hsplit(a,(3,4))   # Split a after the third and the fourth column
+[array([[ 9.,  5.,  6.],
+       [ 1.,  4.,  9.]]), array([[ 3.],
+       [ 2.]]), array([[ 6.,  8.,  0.,  7.,  9.,  7.,  2.,  7.],
+       [ 2.,  1.,  0.,  6.,  2.,  2.,  4.,  0.]])]
+       
+# vsplit splits along the vertical axis, and array_split allows one to specify along which axis to split.
