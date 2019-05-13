28f91ba4c1251b70593313fd28f996582c397904
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-12 22:53:10 -0400

    Create 06. Indexing, slicing and Iterating.py

---
+++ 01. Numpy/06. Indexing, slicing and Iterating.py
@@ -0,0 +1,123 @@
+# Indexing, Slicing and Iterating
+# One-dimensional arrays can be indexed, sliced and iterated over, much like lists and other Python sequences.
+
+>>>
+>>> a = np.arange(10)**3
+>>> a
+array([  0,   1,   8,  27,  64, 125, 216, 343, 512, 729])
+>>> a[2]
+8
+>>> a[2:5]
+array([ 8, 27, 64])
+>>> a[:6:2] = -1000    # equivalent to a[0:6:2] = -1000; from start to position 6, exclusive, set every 2nd element to -1000
+>>> a
+array([-1000,     1, -1000,    27, -1000,   125,   216,   343,   512,   729])
+>>> a[ : :-1]                                 # reversed a
+array([  729,   512,   343,   216,   125, -1000,    27, -1000,     1, -1000])
+>>> for i in a:
+...     print(i**(1/3.))
+...
+nan
+1.0
+nan
+3.0
+nan
+5.0
+6.0
+7.0
+8.0
+9.0
+Multidimensional arrays can have one index per axis. These indices are given in a tuple separated by commas:
+
+>>>
+>>> def f(x,y):
+...     return 10*x+y
+...
+>>> b = np.fromfunction(f,(5,4),dtype=int)
+>>> b
+array([[ 0,  1,  2,  3],
+       [10, 11, 12, 13],
+       [20, 21, 22, 23],
+       [30, 31, 32, 33],
+       [40, 41, 42, 43]])
+>>> b[2,3]
+23
+>>> b[0:5, 1]                       # each row in the second column of b
+array([ 1, 11, 21, 31, 41])
+>>> b[ : ,1]                        # equivalent to the previous example
+array([ 1, 11, 21, 31, 41])
+>>> b[1:3, : ]                      # each column in the second and third row of b
+array([[10, 11, 12, 13],
+       [20, 21, 22, 23]])
+
+
+# When fewer indices are provided than the number of axes, the missing indices are considered complete slices:
+
+>>>
+>>> b[-1]                                  # the last row. Equivalent to b[-1,:]
+array([40, 41, 42, 43])
+
+
+# The expression within brackets in b[i] is treated as an i followed by as many instances of : 
+# as needed to represent the remaining axes. NumPy also allows you to write this using dots as b[i,...].
+
+# The dots (...) represent as many colons as needed to produce a complete indexing tuple. 
+# For example, if x is an array with 5 axes, then
+
+x[1,2,...] is equivalent to x[1,2,:,:,:],
+x[...,3] to x[:,:,:,:,3] and
+x[4,...,5,:] to x[4,:,:,5,:].
+>>>
+>>> c = np.array( [[[  0,  1,  2],               # a 3D array (two stacked 2D arrays)
+...                 [ 10, 12, 13]],
+...                [[100,101,102],
+...                 [110,112,113]]])
+>>> c.shape
+(2, 2, 3)
+>>> c[1,...]                                   # same as c[1,:,:] or c[1]
+array([[100, 101, 102],
+       [110, 112, 113]])
+>>> c[...,2]                                   # same as c[:,:,2]
+array([[  2,  13],
+       [102, 113]])
+
+
+# Iterating over multidimensional arrays is done with respect to the first axis:
+
+>>>
+>>> for row in b:
+...     print(row)
+...
+[0 1 2 3]
+[10 11 12 13]
+[20 21 22 23]
+[30 31 32 33]
+[40 41 42 43]
+
+
+# However, if one wants to perform an operation on each element in the array, 
+# one can use the flat attribute which is an iterator over all the elements of the array:
+>>>
+>>> for element in b.flat:
+...     print(element)
+...
+0
+1
+2
+3
+10
+11
+12
+13
+20
+21
+22
+23
+30
+31
+32
+33
+40
+41
+42
+43
